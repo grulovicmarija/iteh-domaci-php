6051d4000ea47ed15f401063e44a5d1ef2c4e490
Marija Grulovic <grulovicmarija@gmail.com>
2023-01-05 23:40:30 +0100

    Home page

---
+++ home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = 'UTF-8'>
+        <title>MOVIE MENU</title>
+         <!-- <script src="js/jquery-3.3.1.min.js"></script> -->
+        <link rel="stylesheet" type="text/css" href="homeStyle.css">
+    </head>
+    <body>
+        <header class="header">
+            <h1 class="logo">Welcome to cinema!</h1>
+            <!-- <input type="checkbox" id="nav-toggle" class="nav-toggle"> -->
+            <nav class="links">
+                <ul>
+                <li><a href="addMovie.php">Add new movie</a></li>
+                <li><a href="showMovies.php">Show movie base</a></li>
+                <li><a href="posters.php">Movie posters</a></li>
+                <li><a href="deleteMovie.php">Delete movie</a></li>
+                <!-- <li><a href="deleteDirector.php">Remove director</a></li> -->
+                </ul>
+            </nav>
+            <label for="nav-toggle" class="nav-toggle-label"></label>
+        </header>
+    </body>
+</html>
