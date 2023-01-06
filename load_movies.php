1ba7496569cf18465f4c257a1d8b7d8628215a3b
Marija Grulovic <grulovicmarija@gmail.com>
2023-01-06 22:58:53 +0100

    Fancybox and additional improvements to pages

---
+++ load_movies.php
@@ -0,0 +1,14 @@
+<?php
+include 'connection.php';
+$charNewCount = $_POST['charNewCount'];
+$sql = "SELECT * FROM movie LIMIT $charNewCount";
+$result= mysqli_query($mysqli,$sql);
+while($row = mysqli_fetch_assoc($result)){
+	echo $row ['movie_name'];
+    echo '   ';
+    echo $row ['year'];
+    echo '   ';
+	echo $row ['genre'];
+    echo '<br>';	
+}
+?>
